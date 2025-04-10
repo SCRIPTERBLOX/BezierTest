7d1aab4e346dbd9ba9447fd920c8bce503f1aa0a
SCRIPTERBLOX <scriptoblox@gmail.com>
2025-04-10 14:49:13 +0100

    Customization options for size and step size and toggles for first, second and third point and the construction lines

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import pygame
 
+import math
+
 screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("My Game")
 
@@ -18,16 +20,48 @@ drawingPoint2 = [point3[0], point3[1]]
 
 repetitions = 0
 
-pygame.draw.circle(screen, (255, 0, 0), point1, 10)
-pygame.draw.circle(screen, (255, 0, 0), point2, 10)
-pygame.draw.circle(screen, (0, 255, 0), point3, 10)
+sizeSlider = 1
+maxSteps = 400; maxMaxSteps = 800
+enabledFeatures = [
+    False, # Bezier third points
+    False, # Bezier first points
+    False, # Bezier second points
+    False # Bezier construction lines
+]
+
+feature1Debounce = 0
+feature2Debounce = 0
+feature3Debounce = 0
+feature4Debounce = 0
+
+if enabledFeatures[1]:
+    pygame.draw.circle(screen, (255, 0, 0), point1, 10)
+if enabledFeatures[2]:
+    pygame.draw.circle(screen, (255, 0, 0), point2, 10)
+if enabledFeatures[0]:
+    pygame.draw.circle(screen, (0, 255, 0), point3, 10)
 
 currentlySelectedPoint = 0
 currentlyHoldingAPoint = False
 
-sizeSlider = 1
-
 while running:
+    if feature1Debounce > 0:
+        feature1Debounce += 1
+        if feature1Debounce >= 20:
+            feature1Debounce = 0
+    if feature2Debounce > 0:
+        feature2Debounce += 1
+        if feature2Debounce >= 20:
+            feature2Debounce = 0
+    if feature3Debounce > 0:
+        feature3Debounce += 1
+        if feature3Debounce >= 20:
+            feature3Debounce = 0
+    if feature4Debounce > 0:
+        feature4Debounce += 1
+        if feature4Debounce >= 20:
+            feature4Debounce = 0
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -41,11 +75,15 @@ while running:
             drawingPoint1 = [point1[0], point1[1]]
             drawingPoint2 = [point3[0], point3[1]]
 
-            pygame.draw.circle(screen, (255, 0, 0), point1, 10)
-            pygame.draw.circle(screen, (255, 0, 0), point2, 10)
-            pygame.draw.circle(screen, (0, 255, 0), point3, 10)
+            if enabledFeatures[1]:
+                pygame.draw.circle(screen, (255, 0, 0), point1, 10)
+            if enabledFeatures[2]:
+                pygame.draw.circle(screen, (255, 0, 0), point2, 10)
+            if enabledFeatures[0]:
+                pygame.draw.circle(screen, (0, 255, 0), point3, 10)
         else:
             #options
+            # size slider
             if 50-10 <= pygame.mouse.get_pos()[1] <= 50+10:
                 if 600+25 <= pygame.mouse.get_pos()[0] <= 800-25:
                     #get the position in between
@@ -54,6 +92,112 @@ while running:
                     size = rightCorner-leftCorner
                     relativePosition = pygame.mouse.get_pos()[0] - leftCorner
                     sizeSlider = relativePosition / size * 99 + 1
+
+                    pygame.draw.rect(screen, (0, 0, 0), (0, 0, 800, 600))
+                    repetitions = 0
+                    drawingPoint1 = [point1[0], point1[1]]
+                    drawingPoint2 = [point3[0], point3[1]]
+
+                    if enabledFeatures[1]:
+                        pygame.draw.circle(screen, (255, 0, 0), point1, 10)
+                    if enabledFeatures[2]:
+                        pygame.draw.circle(screen, (255, 0, 0), point2, 10)
+                    if enabledFeatures[0]:
+                        pygame.draw.circle(screen, (0, 255, 0), point3, 10)
+
+            # steps slider
+            if 100-10 <= pygame.mouse.get_pos()[1] <= 100+10:
+                if 600+25 <= pygame.mouse.get_pos()[0] <= 800-25:
+                    #get the position in between
+                    rightCorner = 800-25+10
+                    leftCorner = 600+25
+                    size = rightCorner-leftCorner
+                    relativePosition = pygame.mouse.get_pos()[0] - leftCorner
+                    maxSteps = relativePosition / size * (maxMaxSteps-1) + 1
+
+                    pygame.draw.rect(screen, (0, 0, 0), (0, 0, 800, 600))
+                    repetitions = 0
+                    drawingPoint1 = [point1[0], point1[1]]
+                    drawingPoint2 = [point3[0], point3[1]]
+
+                    if enabledFeatures[1]:
+                        pygame.draw.circle(screen, (255, 0, 0), point1, 10)
+                    if enabledFeatures[2]:
+                        pygame.draw.circle(screen, (255, 0, 0), point2, 10)
+                    if enabledFeatures[0]:
+                        pygame.draw.circle(screen, (0, 255, 0), point3, 10)
+
+            # features
+            # third point
+            if 600 + 25 + 10 <= pygame.mouse.get_pos()[0] <= 600 + 25 + 10 + round(((800 - 25) - (600 + 25))/2)-5:
+                if 150 - 10 + 10 <= pygame.mouse.get_pos()[1] <= 150 - 10 + 10 + round(((800 - 25) - (600 + 25))/2)-5:
+                    if not feature1Debounce:
+                        enabledFeatures[0] = not enabledFeatures[0]
+                        feature1Debounce = 1
+                        pygame.draw.rect(screen, (0, 0, 0), (0, 0, 800, 600))
+                        repetitions = 0
+                        drawingPoint1 = [point1[0], point1[1]]
+                        drawingPoint2 = [point3[0], point3[1]]
+
+                        if enabledFeatures[1]:
+                            pygame.draw.circle(screen, (255, 0, 0), point1, 10)
+                        if enabledFeatures[2]:
+                            pygame.draw.circle(screen, (255, 0, 0), point2, 10)
+                        if enabledFeatures[0]:
+                            pygame.draw.circle(screen, (0, 255, 0), point3, 10)
+            # first point
+            if 600 + 25 + 10 + round(((800 - 25) - (600 + 25))/2)+10 <= pygame.mouse.get_pos()[0] <= 600 + 25 + 10 + round(((800 - 25) - (600 + 25))/2)-5 + round(((800 - 25) - (600 + 25))/2)-5:
+                if 150 - 10 + 10 <= pygame.mouse.get_pos()[1] <= 150 - 10 + 10 + round(((800 - 25) - (600 + 25))/2)-5:
+                    if not feature2Debounce:
+                        enabledFeatures[1] = not enabledFeatures[1]
+                        feature2Debounce = 1
+                        pygame.draw.rect(screen, (0, 0, 0), (0, 0, 800, 600))
+                        repetitions = 0
+                        drawingPoint1 = [point1[0], point1[1]]
+                        drawingPoint2 = [point3[0], point3[1]]
+
+                        if enabledFeatures[1]:
+                            pygame.draw.circle(screen, (255, 0, 0), point1, 10)
+                        if enabledFeatures[2]:
+                            pygame.draw.circle(screen, (255, 0, 0), point2, 10)
+                        if enabledFeatures[0]:
+                            pygame.draw.circle(screen, (0, 255, 0), point3, 10)
+
+            # second point
+            if 600 + 25 <= pygame.mouse.get_pos()[0] <= 600 + 25 + round(((800 - 25) - (600 + 25))/2)-5:
+                if 150 - 10 + 10 + round(((800 - 25) - (600 + 25))/2)+10 <= pygame.mouse.get_pos()[1] <= 150 - 10 + 10 + round(((800 - 25) - (600 + 25))/2)-5 + round(((800 - 25) - (600 + 25))/2)-5:
+                    if not feature3Debounce:
+                        enabledFeatures[2] = not enabledFeatures[2]
+                        feature3Debounce = 1
+                        pygame.draw.rect(screen, (0, 0, 0), (0, 0, 800, 600))
+                        repetitions = 0
+                        drawingPoint1 = [point1[0], point1[1]]
+                        drawingPoint2 = [point3[0], point3[1]]
+
+                        if enabledFeatures[1]:
+                            pygame.draw.circle(screen, (255, 0, 0), point1, 10)
+                        if enabledFeatures[2]:
+                            pygame.draw.circle(screen, (255, 0, 0), point2, 10)
+                        if enabledFeatures[0]:
+                            pygame.draw.circle(screen, (0, 255, 0), point3, 10)
+
+            # construction lines
+            if 600 + 25 + round(((800 - 25) - (600 + 25))/2)+10 <= pygame.mouse.get_pos()[0] <= 600 + 25 + round(((800 - 25) - (600 + 25))/2)-5 + round(((800 - 25) - (600 + 25))/2)-5:
+                if 150 - 10 + 10 + round(((800 - 25) - (600 + 25))/2)+10 <= pygame.mouse.get_pos()[1] <= 150 - 10 + 10 + round(((800 - 25) - (600 + 25))/2)-5 + round(((800 - 25) - (600 + 25))/2)-5:
+                    if not feature4Debounce:
+                        enabledFeatures[3] = not enabledFeatures[3]
+                        feature4Debounce = 1
+                        pygame.draw.rect(screen, (0, 0, 0), (0, 0, 800, 600))
+                        repetitions = 0
+                        drawingPoint1 = [point1[0], point1[1]]
+                        drawingPoint2 = [point3[0], point3[1]]
+
+                        if enabledFeatures[1]:
+                            pygame.draw.circle(screen, (255, 0, 0), point1, 10)
+                        if enabledFeatures[2]:
+                            pygame.draw.circle(screen, (255, 0, 0), point2, 10)
+                        if enabledFeatures[0]:
+                            pygame.draw.circle(screen, (0, 255, 0), point3, 10)
     elif pygame.mouse.get_pressed()[2]:
         if pygame.mouse.get_pos()[0] < 600:
             if not currentlyHoldingAPoint:
@@ -69,9 +213,15 @@ while running:
 
             pygame.draw.rect(screen, (0, 0, 0), (0, 0, 800, 600))
 
-            pygame.draw.circle(screen, (255, 0, 0), point1, 10)
-            pygame.draw.circle(screen, (255, 0, 0), point2, 10)
-            pygame.draw.circle(screen, (0, 255, 0), point3, 10)
+            if enabledFeatures[1]:
+                pygame.draw.circle(screen, (255, 0, 0), point1, 10)
+            if enabledFeatures[2]:
+                pygame.draw.circle(screen, (255, 0, 0), point2, 10)
+            if enabledFeatures[0]:
+                pygame.draw.circle(screen, (0, 255, 0), point3, 10)
+            repetitions = 0
+            drawingPoint1 = [point1[0], point1[1]]
+            drawingPoint2 = [point3[0], point3[1]]
     else:
         currentlyHoldingAPoint = False
 
@@ -82,37 +232,79 @@ while running:
     pygame.draw.circle(screen, (0, 255, 0), drawingPoint1, 10)
     pygame.draw.circle(screen, (0, 255, 0), drawingPoint2, 10)"""
 
+    # settings bg
     pygame.draw.rect(screen, (100, 100, 100), (600, 0, 200, 600))
     pygame.draw.rect(screen, (255, 255, 255), (600-5, 0, 10, 600))
 
+    #size slider
     pygame.draw.circle(screen, (255, 255, 255), (600+25, 50), 10)
     pygame.draw.circle(screen, (255, 255, 255), (800-25, 50), 10)
     pygame.draw.rect(screen, (255, 255, 255), (600+25, 50-10, (800-25)-(600+25), 20))
     leftPos = 600+25-10
     rightPos = 800-25+10
-    pygame.draw.circle(screen, (255, 0, 0), (leftPos, 50), 2)
-    pygame.draw.circle(screen, (255, 0, 0), (rightPos, 50), 2)
     pygame.draw.circle(screen, (255, 255, 255), (600 + 25 + ((rightPos-leftPos) / 100 * sizeSlider), 50), 20)
 
-    while repetitions < 400:
-        drawingPoint1[0] += (point3[0] - originalPoint1[0]) / 400
-        drawingPoint1[1] += (point3[1] - originalPoint1[1]) / 400
+    # steps slider
+    pygame.draw.circle(screen, (255, 255, 255), (600 + 25, 100), 10)
+    pygame.draw.circle(screen, (255, 255, 255), (800 - 25, 100), 10)
+    pygame.draw.rect(screen, (255, 255, 255), (600 + 25, 100 - 10, (800 - 25) - (600 + 25), 20))
+    pygame.draw.circle(screen, (255, 255, 255), (600 + 25 + ((rightPos - leftPos) / maxMaxSteps * maxSteps), 100), 20)
 
-        drawingPoint2[0] += -(point3[0] - originalPoint2[0]) / 400
-        drawingPoint2[1] += -(point3[1] - originalPoint2[1]) / 400
+    # features
+    # third point
+    pygame.draw.rect(screen, (255, 255, 255), (600 + 25, 150 - 10, round(((800 - 25) - (600 + 25))/2)-5, round(((800 - 25) - (600 + 25))/2)-5))
+
+    if enabledFeatures[0]:
+        pygame.draw.rect(screen, (0, 255, 0), (600 + 25 + 10, 150 - 10 + 10, round(((800 - 25) - (600 + 25))/2)-5 - 20, round(((800 - 25) - (600 + 25))/2)-5 - 20))
+    else:
+        pygame.draw.rect(screen, (255, 0, 0), (600 + 25 + 10, 150 - 10 + 10, round(((800 - 25) - (600 + 25)) / 2) - 5 - 20, round(((800 - 25) - (600 + 25)) / 2) - 5 - 20))
+
+    # first point
+    pygame.draw.rect(screen, (255, 255, 255), (600 + 25 + round(((800 - 25) - (600 + 25))/2)+10, 150 - 10, round(((800 - 25) - (600 + 25))/2)-5, round(((800 - 25) - (600 + 25))/2)-5))
+
+    if enabledFeatures[1]:
+        pygame.draw.rect(screen, (0, 255, 0), (600 + 25 + 10 + round(((800 - 25) - (600 + 25))/2)+10, 150 - 10 + 10, round(((800 - 25) - (600 + 25))/2)-5 - 20, round(((800 - 25) - (600 + 25))/2)-5 - 20))
+    else:
+        pygame.draw.rect(screen, (255, 0, 0), (600 + 25 + 10 + round(((800 - 25) - (600 + 25)) / 2) + 10, 150 - 10 + 10, round(((800 - 25) - (600 + 25)) / 2) - 5 - 20,  round(((800 - 25) - (600 + 25)) / 2) - 5 - 20))
+
+    # second point
+    pygame.draw.rect(screen, (255, 255, 255), (600 + 25, 150 - 10 + round(((800 - 25) - (600 + 25))/2)+10, round(((800 - 25) - (600 + 25))/2)-5, round(((800 - 25) - (600 + 25))/2)-5))
+
+    if enabledFeatures[2]:
+        pygame.draw.rect(screen, (0, 255, 0), (600 + 25 + 10, 150 - 10 + 10 + round(((800 - 25) - (600 + 25))/2)+10, round(((800 - 25) - (600 + 25))/2)-5 - 20, round(((800 - 25) - (600 + 25))/2)-5 - 20))
+    else:
+        pygame.draw.rect(screen, (255, 0, 0), (600 + 25 + 10, 150 - 10 + 10 + round(((800 - 25) - (600 + 25)) / 2) + 10, round(((800 - 25) - (600 + 25)) / 2) - 5 - 20, round(((800 - 25) - (600 + 25)) / 2) - 5 - 20))
+
+    # construction lines
+    pygame.draw.rect(screen, (255, 255, 255), (600 + 25 + round(((800 - 25) - (600 + 25))/2)+10, 150 - 10 + round(((800 - 25) - (600 + 25))/2)+10, round(((800 - 25) - (600 + 25))/2)-5, round(((800 - 25) - (600 + 25))/2)-5))
+
+    if enabledFeatures[3]:
+        pygame.draw.rect(screen, (0, 255, 0), (600 + 25 + 10 + round(((800 - 25) - (600 + 25))/2)+10, 150 - 10 + 10 + round(((800 - 25) - (600 + 25))/2)+10, round(((800 - 25) - (600 + 25))/2)-5 - 20, round(((800 - 25) - (600 + 25))/2)-5 - 20))
+    else:
+        pygame.draw.rect(screen, (255, 0, 0), (600 + 25 + 10 + round(((800 - 25) - (600 + 25)) / 2) + 10, 150 - 10 + 10 + round(((800 - 25) - (600 + 25)) / 2) + 10, round(((800 - 25) - (600 + 25)) / 2) - 5 - 20, round(((800 - 25) - (600 + 25)) / 2) - 5 - 20))
+
+    while repetitions < maxSteps:
+        drawingPoint1[0] += (point3[0] - originalPoint1[0]) / maxSteps
+        drawingPoint1[1] += (point3[1] - originalPoint1[1]) / maxSteps
+
+        drawingPoint2[0] += -(point3[0] - originalPoint2[0]) / maxSteps
+        drawingPoint2[1] += -(point3[1] - originalPoint2[1]) / maxSteps
+
+        if enabledFeatures[3]: pygame.draw.line(screen, (255, 255, 255), drawingPoint1, drawingPoint2, 2)
+
         repetitions += 1
 
         # draw a circle so far between the two points as respect is to 300
         # so that the circle is closer to point2 when repetitions is close to 300
-        percentage = repetitions / 400
+        percentage = repetitions / maxSteps
         distance = ((drawingPoint1[0] - drawingPoint2[0]) ** 2 + (drawingPoint1[1] - drawingPoint2[1]) ** 2) ** 0.5
         howFarIn = distance/100 * percentage
         #get the position of the line of the two points respecting the howFarIn
         x = drawingPoint1[0] + (drawingPoint2[0] - drawingPoint1[0]) * percentage
         y = drawingPoint1[1] + (drawingPoint2[1] - drawingPoint1[1]) * percentage
-        pygame.draw.circle(screen, (255, 255, 255), (x, y), sizeSlider/1)
-    else:
-        repetitions = 0
+        pygame.draw.circle(screen, (255, 255, 255), (x, y), math.ceil(sizeSlider/15))
+    """else:
+        repetitions = 0""" # this part made some whacky things happen
 
     pygame.display.flip()
     pygame.time.delay(20)
